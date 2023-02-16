fe81d20dab2ad6b1aa29baeaf581710288558aec
Jay-laa <jporsh78@gmail.com>
2023-02-16 05:07:51 -0800

    Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,4 +12,5 @@ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
+
 }
