146bfc2743a130261316f3a85c0a1c4934966bae
Jay-laa <jporsh78@gmail.com>
2023-04-07 07:00:11 -0700

    100-elf_header.c

---
+++ 0x15-file_io/elf_header_info.h
@@ -0,0 +1,23 @@
+#ifndef ELF_HEADER_INFO_H
+#define ELF_HEADER_INFO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <elf.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+#endif /* ELF_HEADER_INFO_H */
