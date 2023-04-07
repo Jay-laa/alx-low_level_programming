a2928abbc669706efbd470e360416c5a80e072c1
Jay-laa <jporsh78@gmail.com>
2023-04-07 05:30:34 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_N
+#define MAIN_H
 
 #include <elf.h>
 #include <stdlib.h>
@@ -15,6 +15,8 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 /**
+ * check_elf - checks if a file is an ELF file
+ * @e_ident: pointer to the ELF identification bytes
  * ELF header functions
  */
 void check_elf(unsigned char *e_ident);
@@ -28,4 +30,4 @@ void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
-#endif
+#endif /* MAIN_H */
