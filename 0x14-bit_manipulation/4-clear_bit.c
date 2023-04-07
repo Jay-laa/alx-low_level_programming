a41bb45aaa999d524443e26b12d72a9f50bf7a02
Jay-laa <jporsh78@gmail.com>
2023-04-07 03:40:15 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,11 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int bit_mask;
+
 	if (index > 63)
 		return (-1);
-	unsigned long int bit_mask = 1UL << index;
-	*n = (*n & ~bit_mask);
+	bit_mask = 1UL << index;
+	*n = (~(bit_mask) & *n);
 	return (1);
 }
